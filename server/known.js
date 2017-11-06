19d5bd409a686c85cb7a30842a58371cb46798e1
Noble Millie <noble.millie@gmail.com>
2017-11-06 16:11:02 -0600

    server folder setup

---
+++ server/known.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var util = require('./lib/utility');
+var partials = require('express-partials');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
+
+var db = require('./app/config');
+var Users = require('./app/collections/users');
+var User = require('./app/models/user');
+var Links = require('./app/collections/links');
+var Link = require('./app/models/link');
+var Click = require('./app/models/click');
+
+var app = express();
