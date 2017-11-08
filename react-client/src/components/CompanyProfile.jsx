306306d7c0d17e4273776cf52980cbfc19252fef
ccm36 <cameron.c.malloy@gmail.com>
2017-11-08 14:56:48 -0600

    Add tag buttons to company profile

--- react-client/src/components/CompanyProfile.jsx
+++ react-client/src/components/CompanyProfile.jsx
@@ -9,17 +9,26 @@ var CompanyProfile = (props) => (
       </h3>
 
       <ul className="company-profile-data">
-        <li>{props.currentCompany.homepage_url}</li>
-        <li>{props.currentCompany.facebook_url}</li>
-        <li>{props.currentCompany.twitter_url}</li>
-        <li>{props.currentCompany.linkedin_url}</li>
-        <li>{props.currentCompany.location_city}</li>
-        <li>{props.currentCompany.location_region}</li>
-        <li>{props.currentCompany.short_description}</li>
-        <li>{props.currentCompany.category_list}</li>
-        <li>{props.currentCompany.funding_total_usd}</li>
-        <li>{props.currentCompany.last_funding_at}</li>
+        <li>Homepage: {props.currentCompany.homepage_url}</li>
+        <li>Facebook: {props.currentCompany.facebook_url}</li>
+        <li>Twitter: {props.currentCompany.twitter_url}</li>
+        <li>LinkedIn: {props.currentCompany.linkedin_url}</li>
+        <li>City: {props.currentCompany.location_city}</li>
+        <li>State: {props.currentCompany.location_region}</li>
+        <li>What we do: {props.currentCompany.short_description}</li>
+        <li>Funding total (2015): {props.currentCompany.funding_total_usd}</li>
+        <li>Most recent funding: {props.currentCompany.last_funding_at}</li>
       </ul>
+
+      <li>
+        Tags: 
+        {(props.currentCompany.category_list)
+          .split('|')
+          .map((tag, i) => 
+            (<button key={i}>{tag}</button>)
+        )}
+      </li>
+
     </div>
   </div>
 );
